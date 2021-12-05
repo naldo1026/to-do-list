95e85286e4aa3dc36f3da4c300e4a2861fdf51e6
Ronaldo Goncalves <rgoncalves1026@gmail.com>
2021-12-05 00:31:18 +0000

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  const firstRender = useRef(true)
+  const [inputValue, setInputValue] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleChange = e => {
+    setInputValue(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    if (inputValue.trim() === '') {
+      
+    } else {
+      setTodos([...todos, {
+        text: inputValue,
+        id: uuidv4(),
+      }])
+    }
+    e.preventDefault()
+    setInputValue('')
+  }
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false
+    } else {
+      localStorage.setItem('TODO', JSON.stringify([...todos]))
+    }
+  }, [todos])
+
+  useEffect(() => {
+    if (localStorage.getItem('TODO') !== null) {
+      const newTodos = localStorage.getItem('TODO')
+      setTodos(JSON.parse([...todos, newTodos]))
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text"
+            placeholder="Next task"
+            value={inputValue}
+            onChange={handleChange}
+          />
+
+          <button type='submit'>Add</button>
+
+        </form>
+        
+        {todos.map((todo) => {
+          return (
+            <div key={todo.id} className='todo'>
+              <p>{todo.text}</p>
+              <i onClick={() => removeTodo(todo.id)} className="far fa-trash-alt"></i>
+            </div>
+          )
+        })}
+
+      </div>
     </div>
   );
 }
